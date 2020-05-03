d72d3eb496463f071bf66e249bf29fa1e9309a73
Francesco D'Izzia <ciccio.dizzia@gmail.com>
2020-05-04 00:37:30 +0200

    some other steps for the huffman trees (still doesn't work)

--- Huffman.py
+++ Huffman.py
@@ -1,7 +1,16 @@
 from Node import *
+import math
+
+def printBinary(x):
+    print("{0:b}".format(x))
+
 
 table = {}
-string = 'she sells seashells by the seashore'
+table2 = {}
+#string = 'she sells seashells by the seashore'
+
+#This is an example because this doesn't work!!!
+string = 'mississippi'
 
 # Aggiungo i caratteri alla table, associando ad ogni lettera il numero di occorrenze
 for char in string:
@@ -13,24 +22,18 @@ for char in string:
 print(table)
 
 sorted_table = {k: v for k, v in sorted(table.items(), key = lambda item: item[1])}
-print(sorted_table)
 
-'''
-a = Node(1, 4)
-b = Node(2, 5)
-c = Node(3, 6)
-
-a.addParent(c) # left(C) = A     (1)
-b.addParent(c) # right(C) = B    (2)
-
-c.PrintTree()
-'''
-
-nodes = []
 N = len(sorted_table)
 print(sorted_table)
-for k,v in sorted_table.items():
-    print(k, end='')
-    nodes.append(Node(k, v))
 
-print(nodes[1].data)
+bits = int(math.log2(N)) + 1 #TOFIX
+
+b = '0' * bits
+print(b)
+
+a = int(b,2)
+for k, v in sorted_table.items():
+    table2[k] = "{0:b}".format(a)
+    a += int('1' ,2)
+
+print(table2)
