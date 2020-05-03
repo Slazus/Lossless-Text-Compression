c51b7e3c5defc0c687fc4c38cfed167f6507eec5
Francesco D'Izzia <ciccio.dizzia@gmail.com>
2020-05-03 03:04:19 +0200

    working on RLE_decode (it's broken, must FIX!)

--- RLE.py
+++ RLE.py
@@ -1,4 +1,4 @@
-def RLE(string):
+def RLE_encode(string):
     count = 1
     lastChar = string[0]
     encoded = ''
@@ -14,5 +14,14 @@ def RLE(string):
     encoded += (str(count) + lastChar)
     return encoded
 
-encoded_text = RLE('aaaaaaabcccddefghhiiiiiiiiiiiiiiiiiiiiiiiillmnooop')
+# Totally broken, MUST FIX!!!
+def RLE_decode(string):
+    for index, _ in enumerate(string):
+        if index % 2 == 0:
+            for _ in range(int(string[index])):
+                print(string[index+1])
+
+encoded_text = RLE_encode('aaaaaaabcccddefghhiiiiiiiiiiiiiiiiiiiiiiiillmnooop')
+
 print(encoded_text)
+#print(RLE_decode(encoded_text))
